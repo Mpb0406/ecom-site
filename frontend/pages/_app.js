12bc83835866b008fcbeac87d86d390193258281
Mike Bolloskis <mpb0406@gmail.com>
2022-07-08 20:45:41 -0400

    Implemented urql provider in _app.js. Created lib/query.js and added products query. Returned products in index.js

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,13 @@
+import { Provider, createClient } from "urql";
+
+const client = createClient({ url: process.env.NEXT_PUBLIC_BACKEND_API });
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider value={client}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
 export default MyApp;
